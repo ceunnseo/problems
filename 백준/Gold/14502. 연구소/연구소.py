6769e4ec17748bc263e8b03aae487dc403185620
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2024-01-16 00:36:16 +0900

    [Gold IV] Title: 연구소, Time: 2564 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -2,52 +2,54 @@ from collections import deque
 from itertools import combinations
 import copy
 n, m = map(int, input().split()) #세로 n 가로 m
-graph = []
-node = [] #벽이 될 수 있는 후보들
-virus = deque([]) #바이러스
+graph = [] #입력받는 그래프
+node = [] #벽이 될 수 있는 후보들 (combinations)
+virus = deque([]) #바이러스 위치를 담을 큐 (bfs)
 cnt = 0 #영역 개수
 area = 0 #최대 크기의 안전영역
 for i in range(n):
     g = list(map(int, input().split()))
     for j in range(m):
-        if g[j] == 0: #빈칸
-            node.append([i, j]) #후보 append
-            cnt += 1
-        elif g[j] == 2: #바이러스
-            virus.append([i, j])
+        if g[j] == 0: #빈칸이면(안전영역)
+            node.append([i, j]) #벽을 세울 후보이다
+            cnt += 1 #안전영역 개수 카운팅
+        elif g[j] == 2: #바이러스라면
+            virus.append([i, j]) #바이러스 담을 큐에 바이러스 위치를 push
     graph.append(g)
-gra = copy.deepcopy(graph) #원본 복사
-v = copy.deepcopy(virus) #원본복사
-def bfs(v, cnt, gra):
+
+# 바이러스 퍼지는 것을 구현하는 bfs 함수
+def bfs(v, cnt, gra): #v : 바이러스가 담긴 큐, cnt : 안전 영역 개수
     while v:
         y, x = v.popleft()
         for (ny, nx) in (y-1, x), (y+1, x), (y, x-1), (y, x+1): #상하좌우
-            if ny < 0 or ny >= n or nx < 0 or nx >= m:
+            if ny < 0 or ny >= n or nx < 0 or nx >= m: #범위 벗어난 경우
                 continue
             if gra[ny][nx] == 0: #바이러스가 퍼질 수 있다면 경우
-                gra[ny][nx] = 2
-                cnt -= 1
-                v.append([ny, nx])
-    return cnt
+                gra[ny][nx] = 2 #바이러스 퍼뜨리고
+                cnt -= 1 #안전영역 1 감소
+                v.append([ny, nx]) #큐에 전염된 바이러스 위치 push
+    return cnt #탐색 끝났을 때의 안전영역을 리턴
 
 
-#print(node)
+#빈칸 위치에서 벽을 세울 3개 조합 만들기
 for k in combinations(node, 3): #k : 튜플형태
-    #print("후보1:",k[0][0], k[0][1], "후보2",k[1][0], k[1][1], "후보3",k[2][0],k[2][1])
-    #print("원본", graph)
-    gra = copy.deepcopy(graph)
     v = copy.deepcopy(virus)
-    #print("바뀌기 전", gra)
-    gra[k[0][0]][k[0][1]] = 1 #빈칸 -> 벽
-    gra[k[1][0]][k[1][1]] = 1 #빈칸 -> 벽
-    gra[k[2][0]][k[2][1]] = 1  # 빈칸 -> 벽
-    #print("graph : ", gra)
-    #print("afer bfs : ")
-    z = bfs(v, cnt-3, gra)
-    if area < z:
-        area = z
-    #print(z)
-    gra[k[0][0]][k[0][1]] = 0 #초기화
-    gra[k[1][0]][k[1][1]] = 0 #초기화
-    gra[k[2][0]][k[2][1]] = 0 #초기화
+    g = copy.deepcopy(graph)  # 그래프 원본 복사
+    #벽 세 개 만들기
+    g[k[0][0]][k[0][1]] = 1
+    g[k[1][0]][k[1][1]] = 1
+    g[k[2][0]][k[2][1]] = 1
+    # 안전 영역에 세 개의 벽을 세우고 (0 -> 1 값 변경)
+
+    z = bfs(v, cnt-3, g) #bfs를 돌려 안전 영역 개수 리턴값을 z에 저장하고
+    # cnt - 3 : 0개수 - 벽의 개수 = 벽 세 개 세웠을 때의 안전영역 개수
+
+    if area < z: #최고기록을 갱신했다면
+        area = z #최고기록 변경
+
+    #초기화
+    g[k[0][0]][k[0][1]] = 0
+    g[k[1][0]][k[1][1]] = 0
+    g[k[2][0]][k[2][1]] = 0
 print(area)
+
