8f89083aa22a58a10b2d2784efcc73e5e7c94db6
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2024-02-02 11:25:05 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+-- 음식 종류별로 묶고
+-- 그 중에서 즐겨찾기수의 최대값을 갖는 식당 출력
+-- 정렬 : 음식 종류 내림차순
+SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+FROM REST_INFO
+WHERE (FOOD_TYPE, FAVORITES) IN (
+    SELECT FOOD_TYPE, MAX(FAVORITES)
+    FROM REST_INFO
+    GROUP BY FOOD_TYPE
+)
+ORDER BY FOOD_TYPE DESC;
