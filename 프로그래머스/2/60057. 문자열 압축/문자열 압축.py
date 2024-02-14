2c97671b8edfec57b3674372034de70c4bd97eeb
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2024-02-14 11:35:55 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,11 +1,11 @@
 def solution(s):
     minimum, n = len(s), len(s)
     for i in range(1, (n//2)+1):
-        start, res, cnt = 0, 0, 1 #압축시작인덱스, 압축결과길이, 동일문자열횟수 
+        start, res, cnt = 0, 0, 1
         while start < n:
-            if s[start:start+i] == s[start+i:start+i+i]: #압축 가능 문자열
+            if s[start:start+i] == s[start+i:start+i+i]:
                 cnt += 1
-            else: #압출불가문자열
+            else:
                 if cnt > 1:
                     res += i + len(str(cnt))
                 else:
