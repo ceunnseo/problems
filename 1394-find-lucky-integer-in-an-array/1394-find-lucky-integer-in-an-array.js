c51441bebf45104e59ec51aad675e48ee21c5f5d
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-07-10 09:36:07 +0900

    Time: 1 ms (92.03%), Space: 54.7 MB (79.23%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -3,22 +3,16 @@
  * @return {number}
  */
 var findLucky = function(arr) {
-    arr.sort((a,b) =>b-a)
-    let freq = 1
-    let prev = arr[0]
-    console.log(arr)
-    for (let i = 1; i<arr.length; i++) {
-        if (prev === arr[i]) {
-            freq += 1;
-        }
-        else if (freq === prev) {
-            return freq
-        }
-        else {
-            freq = 1
-            prev = arr[i]
+    const freq = {};
+    for (const num of arr) {
+        freq[num] = (freq[num] || 0) + 1; //빈도수 저장
+    }
+    let result = -1;
+    for (const key in freq) {
+        const num = parseInt(key);
+        if (freq[key] === num) { //value 값과 Key값이 같은 경우
+            result = Math.max(result, num); //result 를 최대값으로 갱신
         }
     }
-    if (freq === arr[arr.length-1]) return freq
-    return -1
+    return result;
 };
