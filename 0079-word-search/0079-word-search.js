ecee8835eec2bc7b14667f8616dd4320f88c88f0
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-07-21 10:00:21 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -6,19 +6,32 @@
 var exist = function(board, word) {
     const m = board.length;
     const n = board[0].length;
+    const visited = Array.from({length : m}, () => Array(n).fill(false));
+    function* move(r, c) {
+        if (r-1 >= 0) yield [r-1, c];
+        if (r+1 < m) yield [r+1, c];
+        if (c-1 >= 0) yield [r, c-1];
+        if (c+1 < n) yield [r, c+1];
+    }
     const dfs = (r, c, idx) => {
         if (idx === word.length) return true;
-        if (r < 0 || r >= m || c < 0 || c >= n) return false;
-        if (board[r][c] !== word[idx]) return false;
-        const temp = board[r][c];
-        board[r][c] = '#';
-        const result = dfs(r+1, c, idx+1) || dfs(r-1, c, idx+1) || dfs(r, c+1, idx+1) || dfs(r, c-1, idx+1);
-        board[r][c] = temp;
-        return result;
+        if (visited[r][c] || board[r][c] !== word[idx]) return false;
+        visited[r][c] = true;
+        for (const [nr, nc] of move(r, c)) {
+            if (!visited[nr][nc]) {
+                if (dfs(nr, nc, idx+1)) {
+                    return true;
+                }
+            }
+        }
+        visited[r][c] = false;
+        return false;
     }
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (dfs(i, j, 0)) return true;
+            if (board[i][j] === word[0]) {
+                if (dfs(i, j, 0)) return true;
+            }
         }
     }
     return false;
