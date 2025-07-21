6457f193a997b934ae89453e47961656299106d0
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-07-21 09:44:17 +0900

    Time: 2795 ms (5.05%), Space: 63.5 MB (5.43%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -17,14 +17,16 @@ var exist = function(board, word) {
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (board[i][j] === word[0]) {
-                stack.push([i, j, 1]);
+                stack.push([i, j, 1, new Set([`${i},${j}`])]);
                 while (stack.length > 0) {
-                    const [r, c, idx] = stack.pop()
-                    visited[r][c] = true;
+                    const [r, c, idx, visited] = stack.pop()
                     if (idx === word.length) return true;
                     for (const [nr, nc] of move(r, c)) {
-                        if (visited[nr][nc] === false && board[nr][nc] === word[idx]) {
-                            stack.push([nr, nc, idx+1])
+                        const key = `${nr},${nc}`
+                        if (!visited.has(key) && board[nr][nc] === word[idx]) {
+                            const newVisited = new Set(visited);
+                            newVisited.add(key);
+                            stack.push([nr, nc, idx+1, newVisited]);
                         }
                     }
                 }
