c3b0551dace0617d136e97944f547a1150b6466a
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-07-22 10:45:26 +0900

    Time: 78 ms (45.49%), Space: 74.8 MB (21.72%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -9,27 +9,27 @@ var updateMatrix = function(mat) {
     const dist = Array.from({length : m}, () => Array(n).fill(0))
     const queue = []
     const direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    // 0인 위치를 큐에 미리 삽입한다.
-    for (let i = 0; i < m; i++) {
+    for (let i = 0; i < m; i ++) {
         for (let j = 0; j < n; j++) {
             if (mat[i][j] === 0) {
-                queue.push([i, j])
+                queue.push([i, j]) //시작점이 되는 좌표를 넣는다.
                 visited[i][j] = true;
             }
         }
     }
-    //bfs 시작
     while (queue.length > 0) {
         const [r, c] = queue.shift();
         for (const [dr, dc] of direction) {
-            const nr = r + dr
-            const nc = c + dc
-            if (nr >= 0 && nc >= 0 && nr < m && nc < n && !visited[nr][nc]) {
-                dist[nr][nc] = dist[r][c] + 1;
-                visited[nr][nc] = true;
-                queue.push([nr, nc]);
+            const nr = r + dr;
+            const nc = c + dc;
+            if (nr >= 0 && nc >= 0 && nr < m && nc < n) {
+                if (!visited[nr][nc]) {
+                    queue.push([nr, nc])
+                    visited[nr][nc] = true
+                    dist[nr][nc] = dist[r][c] + 1;
+                }
             }
         }
     }
-    return dist;
+    return dist
 };
