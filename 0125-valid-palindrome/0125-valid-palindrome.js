6173e7052f5ba76bff9a3b440b5ca20dde663bee
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-07-23 12:35:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const length = s.length;
+    const maximumIdx = Math.floor(length / 2);
+    console.log("length, maximum", length, maximumIdx)
+};
