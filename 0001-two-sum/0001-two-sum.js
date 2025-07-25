3d8711a1e8d071a845a91e19ce83941c13f89a02
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-07-25 12:17:23 +0900

    Time: 1 ms (87.72%), Space: 54.8 MB (54.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const dic = {}
-    res = []
+    const map = {};
     for (let i = 0; i < nums.length; i++) {
-        dic[nums[i]] = i
-        //console.log(dic[nums[i]])
-    }
-    for (let i = 0; i < nums.length; i++) {
-        let num = target-nums[i];
-        if (dic[num] && dic[num] !== i) {
-            return [i, dic[num]]
+        const complement = target-nums[i];
+        if (complement in map) {
+            return [map[complement], i]
         }
+        map[nums[i]] = i;
     }
 };
