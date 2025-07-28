747cd9c90da6eeea4e0cdfcfa4fc9d42f0a33516
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-07-28 11:26:52 +0900

    Time: 118 ms (12.61%), Space: 59.5 MB (27.03%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.js
+++ 1763-longest-nice-substring/1763-longest-nice-substring.js
@@ -3,22 +3,25 @@
  * @return {string}
  */
 var longestNiceSubstring = function(s) {
-    let longest = ""
-    function isNice(str) {
-        const set = new Set(str);
-        for (let ch of set) {
-            if (!(set.has(ch.toLowerCase()) && set.has(ch.toUpperCase()))) return false;
-        }
-        return true
-    }
-    for (let i = 0; i < s.length; i++) {
+    let longest = '';
+    for (let i = 0; i < s.length; i++ ) {
         for (let j = i+1; j <= s.length; j++) {
             const sub = s.substring(i, j);
-            if (isNice(sub) && (sub.length > longest.length)) {
-                longest = sub;
+            const set = new Set(sub)
+            let flag = false;
+            for (k of set) {
+                if (!(set.has(k.toLowerCase()) && set.has(k.toUpperCase()))) {
+                    flag = true ;
+                    break;
+                }
             }
+            if (!flag) {
+                if (longest.length < sub.length) {
+                    longest = sub;
+                }
+            }
+            
         }
     }
     return longest;
-    
 };
