36a6ddf8be3a92848ccfa7960d3d3cd9ae67da4c
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-01 11:06:05 +0900

    Time: 0 ms (100%), Space: 55.3 MB (5.8%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.js
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.js
@@ -3,29 +3,20 @@
  * @return {string}
  */
 var reorderSpaces = function(text) {
-    let space = 0;
-    let word = ''
-    let wordList = []
-    //공백과 단어 세기
-    for (t of text) {
-        if (t === ' ') {
-            space += 1
-            if (word !== '') {
-                wordList.push(word)
-                word = ''
-            }
+    let newTextArray = text.split(" ").filter((x) => x.length > 0);
+    let totalSpaces = text.length - newTextArray.join().replaceAll(',','').length
+    let spaceDistribution = Math.floor(totalSpaces / (newTextArray.length - 1));
+    const char = ' ';
+    let newText = '';
+    let spaceCounter = 0;
+    for (let i = 0; i < newTextArray.length; i++) {
+        newText += newTextArray[i];
+        if (i === newTextArray.length-1) {
+            newText += char.repeat(totalSpaces - spaceCounter);
+            break;
         }
-        else {
-            word += t
-        }
+        spaceCounter = spaceCounter + spaceDistribution;
+        newText += char.repeat(spaceDistribution);
     }
-    if (word !== '') wordList.push(word)
-
-    //공백 붙이기
-    if (wordList.length === 1) {
-        return wordList[0] + ' '.repeat(space)
-    }
-    const betweenSpace = Math.floor(space / (wordList.length-1))
-    const lastSpace = (space % (wordList.length - 1))
-    return wordList.join(' '.repeat(betweenSpace)) + ' '.repeat(lastSpace)
+    return newText
 };
