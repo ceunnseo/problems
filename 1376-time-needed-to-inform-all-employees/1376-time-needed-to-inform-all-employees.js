b035b9021d7962719c080b02283a196991383a0f
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-04 18:54:32 +0900

    Time: 1289 ms (6.67%), Space: 98.4 MB (44.17%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.js
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.js
@@ -5,21 +5,25 @@
  * @param {number[]} informTime
  * @return {number}
  */
+ //전염병 확산과 같은 문제
 var numOfMinutes = function(n, headID, manager, informTime) {
-    const tree = new Map()
-    for (let i = 0; i < n ; i++) {
-        if (!tree.has(manager[i])) tree.set(manager[i], [])
-        tree.get(manager[i]).push(i)
-    }
-    // -1 -> [2] 2 -> [0, 1, 3, 4, 5]
+    const queue = [[headID, informTime[headID]]] //start, 누적 time
+    const tree = new Map();
     let maxTime = 0;
-    const queue = [[headID, 0]] //직원번호, 누적 시간
+    manager.forEach((parent, node) => {
+        if (!tree.has(parent)) {
+            tree.set(parent, []);
+        }
+        tree.get(parent).push(node)
+    })
     while (queue.length) {
-        const [id, time] = queue.shift();
-        maxTime = Math.max(maxTime, time);
-        if (tree.has(id)) {
-            for (const sub of tree.get(id)) {
-                queue.push([sub, time + informTime[id]])
+        const [node, time] = queue.shift();
+        console.log('node', node, 'time', time)
+        maxTime = Math.max(maxTime, time)
+        if (tree.has(node)) {
+            for (t of tree.get(node)) { //자식들
+                //console.log('informTime', informTime[t], t)
+                queue.push([t, time + informTime[t]])
             }
         }
     }
