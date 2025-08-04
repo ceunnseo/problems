2ab70ac72769d22dba116296c9670adde0820aaa
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-04 19:29:07 +0900

    Time: 0 ms (100%), Space: 53.4 MB (73.97%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.js
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.js
@@ -3,15 +3,10 @@
  * @return {string}
  */
 var reorderSpaces = function(text) {
-    const textList = text.split(' ').filter((x) => x.length > 0)
-    const spaces = text.length - textList.join().replaceAll(',', '').length
-    if (textList.length === 1) return textList[0] + ' '.repeat(spaces);
-    const spaceEvenly = Math.floor(spaces / (textList.length - 1));
-    const spaceLastly = spaces - (spaceEvenly * (textList.length-1));
-    let newText = '';
-    for (let i = 0; i < textList.length; i++) {
-        if (i === textList.length-1) newText += textList[i] + ' '.repeat(spaceLastly)
-        else newText += textList[i] + ' '.repeat(spaceEvenly)
-    }
-    return newText;
+    const wordList = text.split(' ').filter((x) => x.length > 0)
+    const space = text.length - wordList.join().replaceAll(',', '').length
+    if (wordList.length === 1) return wordList + ' '.repeat(space)
+    const evenlySpace = Math.floor(space / (wordList.length - 1))
+    const otherSpace = space - (evenlySpace * (wordList.length-1))
+    return wordList.join(' '.repeat(evenlySpace)) + ' '.repeat(otherSpace)
 };
