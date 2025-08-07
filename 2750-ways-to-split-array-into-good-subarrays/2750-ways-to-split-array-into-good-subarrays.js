7b8017009b073fe8c2a7ae167590f5913e1779bb
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-07 10:24:46 +0900

    Time: 106 ms (0%), Space: 78 MB (12.5%) - LeetHub

--- 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.js
+++ 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.js
@@ -2,6 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// subarray : 1이 하나만 있는 부분 배열
+// 1 0 1 -> 나눌 수 없음
+
+// 1 1 0 -> 1, 1 0
+
+// 0 1 0 0 1 -> 0 1, 0 0 1 / 0 1 0, 0 1 / 0 1 0 0, 1
+
+// 0 1 0 0 1 0 1
+// 0 1 , 0 0 1, 0 1 / 0 1 0, 0 1, 0 1 / 0 1 0 0, 1, 0 1
+// 0 1 , 0 0 1 0, 1 / 0 1 0, 0 1 0, 1 / 0 1 0 0, 1 0, 1
 var numberOfGoodSubarraySplits = function(nums) {
     const res = []
     let isZero = 0
@@ -18,12 +29,12 @@ var numberOfGoodSubarraySplits = function(nums) {
             }
         }
     }
-    if (!isCountable) return 0;
-    if (res.length === 0) return 1;
-    let result = 1n;
+    if (!isCountable) return 0; //1이 나온 적이 없을 때
+    if (res.length === 0) return 1; //1은 나왔지만 1과 1사이 자를 수 없을 때
+    let result = 1n; //n을 붙여서 BigInt 타입
     for (const c of res) {
         result *= BigInt(c+1);
     }
     const MOD = 1000000007n;
-    return Number(result % MOD);
+    return Number(result % MOD); //숫자가 너무 큰 경우 10^9 + 7 나눈 나머지 
 };
