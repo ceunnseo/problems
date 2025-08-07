c064d551937cacdaaf739ad8aa93776d58bcfc3f
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-07 23:22:21 +0900

    Time: 371 ms (41.98%), Space: 54 MB (82.82%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -6,26 +6,29 @@
 var exist = function(board, word) {
     const m = board.length;
     const n = board[0].length;
-    const visited = Array.from({length : m}, () => Array(n).fill(false));
-    function* move(r, c) {
-        if (r-1 >= 0) yield [r-1, c];
-        if (r+1 < m) yield [r+1, c];
-        if (c-1 >= 0) yield [r, c-1];
-        if (c+1 < n) yield [r, c+1];
+    const visited = Array.from({length : m}, () => Array(n).fill(false))
+    const dy = [0, 0, -1, 1] //상하좌우
+    const dx = [-1, 1, 0, 0]
+
+    function dfs (i, j, idx) {
+        if (idx === word.length) return true; //모든 글자를 찾았을 때
+        if (i < 0 || i >= m || j < 0 || j >=n) return false; //범위 넘어감
+        if (visited[i][j] || board[i][j] !== word[idx]) return false;
+        visited[i][j] = true;
+        for (let d = 0; d < 4; d++) {
+            const nx = i + dx[d];
+            const ny = j + dy[d];
+            if (dfs(nx, ny, idx+1)) return true;
+        }
+        visited[i][j] = false;
+        return false;
     }
-    const dfs = (r, c, idx) => {
-        if (idx === word.length) return true;
-        if (r < 0 || r >= m || c < 0 || c >= n) return false;
-        if (visited[r][c] || board[r][c] !== word[idx]) return false;
-        visited[r][c] = true;
-        const result = dfs(r-1, c, idx+1) || dfs(r+1, c , idx + 1) || dfs(r, c-1, idx+1) || dfs(r, c+1, idx+1)
-        visited[r][c] = false;
-        return result;
-    }
+
+
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (board[i][j] === word[0]) {
-                if (dfs(i, j, 0)) return true;
+            if (board[i][j] === word[0]) {//첫글자 발견한 경우 -> 탐색 시작 
+                if (dfs(i, j, 0)) return true
             }
         }
     }
