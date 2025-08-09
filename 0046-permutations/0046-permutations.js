899b31d3b4dfd3d9810dc03fb790be62a403a25d
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-10 01:17:14 +0900

    Time: 0 ms (100%), Space: 60.2 MB (28.84%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,22 +3,23 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    const result = []
-    const used = Array(nums.length).fill(false)
-    function backtrack(path) {
+    const result = [];
+    const visited = Array(nums.length).fill(false);
+    function dfs(start, path) {
         if (path.length === nums.length) {
-            result.push([...path])
-            return
+            result.push([...path]);
+            return;
         }
         for (let i = 0; i < nums.length; i++) {
-            if (used[i] === true) continue;
-            used[i] = true
-            path.push(nums[i]);
-            backtrack(path)
-            path.pop();
-            used[i] = false;
-        }
+            if (!visited[i]) {
+                visited[i] = true;
+                path.push(nums[i]); //[1]
+                dfs(i+1, path); //3, [1, 2, 3]
+                visited[i] = false;
+                path.pop()
+            }            
+        }   
     }
-    backtrack([])
-    return result
+    dfs(0, []);
+    return result;
 };
