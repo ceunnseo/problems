ae11bc6e011ae523360a7f18c72b389b04f4a3ad
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-09 19:50:47 +0900

    Time: 18 ms (9.79%), Space: 62 MB (19.26%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -8,20 +8,21 @@ var combinationSum = function(candidates, target) {
     let subsets = new Set();
     let dict = {};
     let result = [];
-    function dfs(total, path) {
+    function dfs(total,startIdx,path) {
         if (total > target) return;
         if (total === target) {
             dict[[...path].sort((a,b) => a-b)] = 1;
         }
-        for (let i = 0; i < candidates.length; i++) {
+        for (let i = startIdx; i < candidates.length; i++) {
+            if (target-total < candidates[i]) continue; //탐색하지 않는다.
             let res = total + candidates[i];
             let paths = [...path];
             paths.push(candidates[i])
-            dfs(res, paths);
+            dfs(res, i, paths);
             paths.pop();
         }
     }
-    dfs(0, [])
+    dfs(0, 0, [])
     console.log(subsets)
     const answer = Object.keys(dict);
     const realAnswer = [];
