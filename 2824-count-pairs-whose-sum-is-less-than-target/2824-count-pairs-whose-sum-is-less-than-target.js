b9c61da3c3a7f01734b8583c9a55562b43dd3ab7
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-08-27 11:18:46 +0900

    Time: 2 ms (28.67%), Space: 55.8 MB (47.78%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -4,13 +4,18 @@
  * @return {number}
  */
 var countPairs = function(nums, target) {
+    nums.sort((a,b) => a-b)
+    let i = 0, j = nums.length-1;
     let cnt = 0;
-    nums.sort((a,b) => a - b);
-    for (let i = 0; i < nums.length-1; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] >= target) break;
-            cnt += 1;
+    while (i < j) {
+        if (nums[i] + nums[j] < target) {
+            cnt += (j-i);
+            i++;
+        }
+        else {
+            j--;
         }
     }
     return cnt;
+    
 };
