98ad1807f3aeee98262750612d7c2bf6ffcfa109
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-09-04 10:04:38 +0900

    Time: 73 ms (5.17%), Space: 65.5 MB (5.37%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.js
+++ 3606-coupon-code-validator/3606-coupon-code-validator.js
@@ -7,27 +7,32 @@
 //code : a-z, A-Z, 0-9, _ 만 가능
 //businessLine : 'electronics', 'grocery', 'pharmacy', 'restaurant'
 var validateCoupons = function(code, businessLine, isActive) {
-    const dict = {};
+    const businessMap = new Map();
     const businessList = ['electronics', 'grocery', 'pharmacy', 'restaurant']
     const n = code.length;
     const regex = /^[a-zA-Z0-9_]+$/;
     const res = [];
+
     for (const b of businessList) {
-        dict[b] = [];
+        businessMap.set(b, []);
     }
+    //key : electronices -> value : [], ...
+
     for (let i = 0; i < n; i++) {
         if(regex.test(code[i]) && isActive[i]) {
-            if (dict[businessLine[i]]) {
-                dict[businessLine[i]].push(code[i])
+            const currentBusiness = businessLine[i];
+            if (businessMap.has(currentBusiness)) {
+                businessMap.get(currentBusiness).push(code[i])
             }
         }
     }
-    const t = Object.values(dict).filter((x) => x.length > 0 && x.sort());
-    for (let i = 0; i < t.length; i++) {
-        for (let j = 0; j < t[i].length; j++) {
-            res.push(t[i][j])
+    for (const b of businessList) {
+        const coupons = businessMap.get(b);
+        console.log(b, coupons);
+        if (coupons.length > 0) {
+            coupons.sort();
+            res.push(...coupons);
         }
     }
     return res;
-
 };
