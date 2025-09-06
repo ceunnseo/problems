274de64b2595e6b3201b5586d32e1a793432eae4
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-09-06 11:36:42 +0900

    Time: 0 ms (100%), Space: 55 MB (11.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.js
+++ 0520-detect-capital/0520-detect-capital.js
@@ -3,7 +3,11 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-    return /^[A-Z]+$/.test(word)
-    || /^[a-z]+$/.test(word)
-    || /^[A-Z][a-z]+$/.test(word);
+    //조건1. 문자열의 시작과 끝이 모두 대문자만
+    //조건2. 문자열의 시작과 끝이 모두 소문자만
+    //조건3. 앞에만 대문자, 이후는 소문자
+    const regexp1 = /^[A-Z]+$/;
+    const regexp2 = /^[a-z]+$/;
+    const regexp3 = /^[A-Z][a-z]+$/;
+    return regexp1.test(word) || regexp2.test(word) || regexp3.test(word);
 };
