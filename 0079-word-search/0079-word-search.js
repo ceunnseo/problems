1fcac5e110b681430a836c8787ce65ebf66e9d13
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-09-25 07:02:25 +0900

    Time: 257 ms (87.95%), Space: 54.9 MB (62.18%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -6,31 +6,37 @@
 var exist = function(board, word) {
     const m = board.length;
     const n = board[0].length;
-    const visited = Array.from({length : m}, () => Array(n).fill(false))
-    const dy = [0, 0, -1, 1] //상하좌우
-    const dx = [-1, 1, 0, 0]
-
-    function dfs (i, j, idx) {
-        if (idx === word.length) return true; //모든 글자를 찾았을 때
-        if (i < 0 || i >= m || j < 0 || j >=n) return false; //범위 넘어감
-        if (visited[i][j] || board[i][j] !== word[idx]) return false;
-        visited[i][j] = true;
-        for (let d = 0; d < 4; d++) {
-            const nx = i + dx[d];
-            const ny = j + dy[d];
-            if (dfs(nx, ny, idx+1)) return true;
+    const dr = [-1, 1, 0, 0];
+    const dc = [0, 0, -1, 1];
+    const visited = Array.from({length : m}, () => Array(n).fill(false));
+    function dfs(r, c, idx) {
+        if (r < 0 || r >= m || c < 0 || c >= n) return;
+        if (idx >= word.length) {
+            return true;
         }
-        visited[i][j] = false;
-        return false;
+        //console.log('dfs 실행', r, c, idx)
+        for (let i = 0; i < 4; i++) {
+            const nr =  r + dr[i];
+            const nc = c + dc[i];
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                //console.log('후보', nr, nc, visited[nr][nc])
+                if (!visited[nr][nc] && board[nr][nc] === word[idx]) {
+                    visited[nr][nc] = true;
+                    if (dfs(nr, nc, idx+1)) return true;
+                    visited[nr][nc] = false;
+                }
+            }
+        }
     }
-
-
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (board[i][j] === word[0]) {//첫글자 발견한 경우 -> 탐색 시작 
-                if (dfs(i, j, 0)) return true
+            if (board[i][j] === word[0]) {
+                visited[i][j] = true;
+                if (dfs(i, j, 1)) return true;
+                visited[i][j] = false;
             }
         }
     }
     return false;
+    
 };
