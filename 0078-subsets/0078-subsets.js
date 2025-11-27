370b8a18fff421bb2ecc6616ce4e37d18e35ab46
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-11-27 10:51:44 +0900

    Time: 1 ms (7.87%), Space: 59.3 MB (5.23%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,15 +3,21 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
+    const n = nums.length;
+    const total = 1 << n; //2^n : 1이라는 숫자를 2진수로 보고 왼쪽으로 n번 이동시킨다.
+    // 1<<n 자체가 n번째 비트만 1인 숫자를 만드는 것
+    //n=3이면 0001 -> 1000 (10진수로는 8)
     const result = [];
-    function dfs(path, idx) { //base case : for 때문에 작성하지 않아도 됨
-        result.push([...path])
-        for (let i = idx; i < nums.length; i++) {
-            path.push(nums[i]);
-            dfs(path, i+1); 
-            path.pop();
+    // 000, 001, 010, 011, 100, 101, 110, 111 까지만
+    for (let bitMask = 0; bitMask < total; bitMask++) {
+        subset = [];
+        for (let j = 0; j < n; j++) {
+            //j=0 -> 1<<j 이므로 001 -> 101
+            if (bitMask & (1<<j)) {
+                subset.push(nums[j]);
+            }
         }
+        result.push(subset);
     }
-    dfs([], 0); //처음, 시작할 인덱스
     return result;
 };
