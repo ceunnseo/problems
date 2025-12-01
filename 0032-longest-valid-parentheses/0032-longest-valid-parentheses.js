4be0dc9b76e82b8c4032bd5009bc9b243fb93356
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-12-01 16:48:56 +0900

    Time: 7 ms (25.38%), Space: 62 MB (7.18%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    const n = s.length;
+    const visited = Array(n).fill(false);
+    const stack = [];
+    let cnt = 0;
+    let maximum = 0;
+    for (let i = 0; i < n; i++) {
+        if (s[i] === '(') stack.push(i);
+        else {
+            if (stack.length) {
+                const k = stack.pop();
+                visited[i] = true;
+                visited[k] = true;
+            }
+        }
+    }
+    for (const v of visited) {
+        if (v) cnt +=1 ;
+        else cnt = 0;
+        maximum = Math.max(maximum, cnt);
+    }
+    return maximum;
+};
