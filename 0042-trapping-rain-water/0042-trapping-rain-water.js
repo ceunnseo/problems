9e5f001ebcd8f597570d8481d7bac2836e3d431b
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2025-12-01 09:50:03 +0900

    Time: 2 ms (50.48%), Space: 55.9 MB (96.7%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    if (height.length <= 2) return 0; //고일 수가 없는 구조
+    let result = 0;
+    const stack =[];
+    for (let i = 0; i < height.length; i++) {
+        while (stack.length && height[stack[stack.length-1]] <= height[i]) {
+            const bottomIdx = stack.pop(); 
+            const bottomHeight = height[bottomIdx];
+            if (stack.length === 0) break; //왼쪽 벽이 없음
+            const leftIdx = stack[stack.length-1];
+            const leftHeight = height[leftIdx];
+            result += [Math.min(leftHeight, height[i]) - bottomHeight] * (i-leftIdx-1);
+        }
+        stack.push(i);
+    }
+    return result
+};
