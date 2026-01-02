eec560cadfe75a49efa21e837441a533cddf1562
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2026-01-02 22:59:18 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const sets = new Set(nums);
+    let length = 0;
+    for (let num of nums) {
+        if (!sets.has(num-1)) { //num-1이 sets에 없으면 => 시작점이 될 수 있음
+            let current = num
+            let cnt = 0;
+            while (sets.has(current)) {
+                cnt+=1;
+                current+=1;
+            }
+            length = Math.max(length, cnt);
+        }
+    }
+    return length;
+};
