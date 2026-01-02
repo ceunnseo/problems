5674504005e552926c53b2160c10ac0bba71711e
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2026-01-02 23:21:21 +0900

    Time: 31 ms (95.81%), Space: 80.5 MB (44.28%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -5,7 +5,7 @@
 var longestConsecutive = function(nums) {
     const sets = new Set(nums);
     let length = 0;
-    for (let num of nums) {
+    for (let num of sets) {
         if (!sets.has(num-1)) { //num-1이 sets에 없으면 => 시작점이 될 수 있음
             let current = num
             let cnt = 0;
