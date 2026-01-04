6e0fa46fe3a73920b9770e6de2205b466de0d68d
Eunseo Choi <59520918+ceunnseo@users.noreply.github.com>
2026-01-04 10:58:37 +0900

    Time: 17 ms (28.7%), Space: 58.6 MB (45.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -10,24 +10,23 @@ var lengthOfLongestSubstring = function(s) {
     let right = left;
     while (right < s.length) {
         if (!(s[right] in dict)) {
-            console.log('중복이 없음', left, right)
+            //console.log('중복이 없음', left, right)
             dict[s[right]] = right;
             right++;
             length = Math.max(length, right-left);
         }
         else { //중복 문자를 만난 경우
-            console.log('중복 발생', left, right) 
+            //console.log('중복 발생', left, right) 
             //left = dict[s[right]] + 1; //left : 중복이 일어나지 않은 위치로
             if (dict[s[right]] >= left) {
-                left = Math.max(left, dict[s[right]] + 1);
-                //left = dict[s[right]] + 1;
+                left = dict[s[right]] + 1;
             }
             dict[s[right]] = right; //마지막으로 나온 중복 단어 인덱스 갱신
             right++;
             length = Math.max(length, right-left);
         }
-        console.log('left', left, 'right', right, 'length', length);
+        //console.log('left', left, 'right', right, 'length', length);
     }
-    console.log('=======length=======', length);
+    //console.log('=======length=======', length);
     return length;
 };
